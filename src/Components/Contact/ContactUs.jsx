50fb4a01ca2a1c7c217046ace5f538c96fc05f47
LeonardFLY <leonard.alvesm@outlook.com>
2022-03-18 15:23:08 -0300

    corrigindo deploy error em contactUs

--- src/Components/Contact/ContactUs.jsx
+++ src/Components/Contact/ContactUs.jsx
@@ -1,155 +1,111 @@
-import React from 'react'
-import { useState } from 'react'
-import './ContactUs.css'
-import emailjs from '@emailjs/browser'
-import { useForm } from 'react-hook-form'
+import { useForm } from 'react-hook-form';
 
-export default () => {
-    const [successMessage, setSuccessMessage] = useState("");
-    const { register, handleSubmit, formState: { errors } } = useForm();
+const ContactForm = () => {
+  const {
+    register,
+    handleSubmit,
+    reset,
+    formState: { errors }
+  } = useForm();
+  
+  const onSubmit = async (data) => {
+    const { name, email, subject, message } = data;
+    
+    console.log('Nome: ', name);
+    console.log('Email: ', email);
+    console.log('Assunto: ', subject);
+    console.log('Mensagem: ', message);
+  };
 
-    const serviceID = '---';
-    const templateID = '---';
-    const userID = '---';
+  return (
+    <div className='ContactForm mt-5 mb-3'>
+      <div className='container'>
+        <div className='row'>
+          <div className='col-12 text-center'>
+            <div className='contactForm'>
+              <form id='contact-form' onSubmit={handleSubmit(onSubmit)} noValidate>
 
-    const onSubmit = (data, r) => {
-        sendEmail(
-            serviceID,
-            templateID,
-            {
-                nome: data.nome,
-                telefone: data.telefone,
-                email: data.email,
-                assunto: data.assunto,
-                mensagem: data.mensagem
-            },
-            userID
-        )
-        r.target.reset();
-    }
+                {/* form name e email*/}
+                <div className='row formRow'>
+                  <div className='col-6'>
+                    <input
+                      type='text'
+                      name='name'
+                      {...register('name', {
+                        required: { value: true, message: 'Por favor, insira o seu nome' },
+                        maxLength: {
+                          value: 30,
+                          message: 'Por favor, utilize 30 caracteres ou mais'
+                        }
+                      })}
+                      className='form-control formInput'
+                      placeholder='Nome'
+                    ></input>
+                    {errors.name && <span className='errorMessage'>{errors.name.message}</span>}
+                  </div>
+                  <div className='col-6'>
+                    <input
+                      type='email'
+                      name='email'
+                      {...register('email', {
+                        required: true,
+                        pattern: /^[a-zA-Z0-9.!#$%&’*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/
+                      })}
+                      className='form-control formInput'
+                      placeholder='Email'
+                    ></input>
+                    {errors.email && (
+                      <span className='errorMessage'>Por favoz, insira um endereço de e-mail válido</span>
+                    )}
+                  </div>
+                </div>
 
-    const sendEmail = (serviceID, templateID, variables, userID) => {
-        emailjs.send(serviceID, templateID, variables, userID)
-            .then(() => {
-                setSuccessMessage('Mensagem enviada com sucesso! Entraremos em contato assim que possível.');
-            }).catch(err => console.error(`Algo deu errado ${err}`));
-    };
+                {/* form assunto */}
+                <div className='row formRow'>
+                  <div className='col'>
+                    <input
+                      type='text'
+                      name='subject'
+                      {...register('subject', {
+                        required: { value: true, message: 'Insira o assunto desejado' },
+                        maxLength: {
+                          value: 70,
+                          message: 'Por favor, insira um assunto de até 70 caracteres'
+                        }
+                      })}
+                      className='form-control formInput'
+                      placeholder='Assunto'
+                    ></input>
+                    {errors.subject && (
+                      <span className='errorMessage'>{errors.subject.message}</span>
+                    )}
+                  </div>
+                </div>
 
-    return (
-        <div id='contato' className='contato'>
-            <div className='text-center'>
-                <h1>Contato</h1>
-                <span className="success-message">{successMessage}</span>
+                {/* form message */}
+                <div className='row formRow mb-2'>
+                  <div className='col'>
+                    <textarea
+                      rows={3}
+                      name='message'
+                      {...register('message', {
+                      })}
+                      className='form-control formInput'
+                      placeholder='Mensagem'
+                    ></textarea>
+                    {errors.message && <span className='errorMessage'>Insira aqui a sua mensagem</span>}
+                  </div>
+                </div>
+                <button className='submit-btn' type='submit'>
+                  Enviar
+                </button>
+              </form>
             </div>
-            <div className='container'>
-                <form onSubmit={handleSubmit(onSubmit)}>
-                    <div className='row'>
-                        <div className='col-md-6 col-xs-12 '>
-                            {/* NOME INPUT */}
-                            <div className='text-center'>
-                                <input
-                                    type='text'
-                                    className='form-control'
-                                    placeholder='Nome'
-                                    name='nome'
-                                    aria-invalid={errors.nome ? "true" : "false"}
-                                    {...register("nome", {
-                                        required: "Por favor, digite seu nome",
-                                        maxLength: {
-                                            value: 20,
-                                            message:
-                                                "Por favor, fornaça um nome com menos de 20 caracteres",
-                                        },
-                                    })}
-                                />
-                                <div className='line'></div>
-                            </div>
-                            <span className='error-message'>
-                                {errors.nome && errors.nome.message}
-                            </span>
-                            {/* TELEFONE INPUT */}
-                            <div className='text-center'>
-                                <input
-                                    type='text'
-                                    className='form-control'
-                                    placeholder='Telefone'
-                                    phone='tefefone'
-                                    aria-invalid={errors.telefone ? "true" : "false"}
-                                    {...register('telefone',{
-                                            required: 'Por favor, insira o número do seu telefone',
-                                        })
-                                    }
-                                />
-                                <div className='line'></div>
-                            </div>
-                            <span className="error-message">
-                                {errors.telefone && errors.telefone.message}
-                            </span>
-                            {/* EMAIL INPUT */}
-                            <div className='text-center'>
-                                <input
-                                    type='email'
-                                    className='form-control'
-                                    placeholder='E-mail'
-                                    name='email'
-                                    aria-invalid={errors.email ? "true" : "false"}
-                                    {...register('email', {
-                                            required: "Por favor, forneça seu email!",
-                                            pattern: {
-                                                value: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i,
-                                                message: "Email inválido"
-                                            }
-                                        })
-                                    }
-                                />
-                                <div className='line'></div>
-                            </div>
-                            <span className="error-message">
-                                {errors.email && errors.email.message}
-                            </span>
-                            {/* ASSUNTO INPUT */}
-                            <div className='text-center'>
-                                <input
-                                    type='text'
-                                    className='form-control'
-                                    placeholder='Assunto'
-                                    name='assunto'
-                                    aria-invalid={errors.assunto ? "true" : "false"}
-                                    {...register('assunto', {
-                                            required: 'OOPS, você esqueceu de adicionar um assunto',
-                                        })
-                                    }
-                                />
-                                <div className='line'></div>
-                            </div>
-                            <span className="error-message">
-                                {errors.assunto && errors.assunto.message}
-                            </span>
-                        </div>
-                        <div className='col-md-6 col-sm-12'>
-                            {/* MENSAGEM */}
-                            <div className='text-center'>
-                                <textarea
-                                    type='text'
-                                    className='form-control'
-                                    placeholder='Digite aqui a sua mensagem'
-                                    name='mensagem'
-                                    aria-invalid={errors.mensagem ? "true" : "false"}
-                                    {...register('mensagem',{
-                                            required: "Por favor, digite aqui a sua mensagem...",
-                                        })
-                                    }
-                                ></textarea>
-                                <div className='line'></div>
-                            </div>
-                            <span className="error-message">
-                                {errors.mensagem && errors.mensagem.message}
-                            </span>
-                            <button className='btn-contato contact-btn' type='submit'>ENVIAR</button>
-                        </div>
-                    </div>
-                </form>
-            </div>
+          </div>
         </div>
-    )
-}
+      </div>
+    </div>
+  );
+};
+
+export default ContactForm;
