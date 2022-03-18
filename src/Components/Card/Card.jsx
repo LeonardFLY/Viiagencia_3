126309f19b3549784e096f34e0f5ac6ab3702a91
LeonardFLY <leonard.alvesm@outlook.com>
2022-03-18 13:18:12 -0300

    adicionando alt aos cards

--- src/Components/Card/Card.jsx
+++ src/Components/Card/Card.jsx
@@ -5,7 +5,7 @@ function Card(props) {
         <React.Fragment>
             <div className="card col-sm-3 col-lg-6">
                 <h4>{props.Estado} - {props.Cidade}</h4>
-                <img className="card-img" src={props.Picture}/>
+                <img className="card-img" src={props.Picture} alt={'Imagem de' + props.Cidade}/>
                 <div>
                     <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Placeat itaque neque iure nostrum eius nemo, officiis nisi saepe delectus, cum est unde maxime voluptate quibusdam explicabo culpa a quos earum.</p>
                 
