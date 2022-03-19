22f81c9b5cd518112d4d816a78d0f8f5e1b5dae1
LeonardFLY <leonard.alvesm@outlook.com>
2022-03-18 23:54:26 -0300

    projeto finalizado, entrega 18/03

--- src/Components/Card/Card.jsx
+++ src/Components/Card/Card.jsx
@@ -4,7 +4,8 @@ function Card(props) {
     return (
         <React.Fragment>
             <div className="card col-sm-3 col-lg-6">
-                <h4>{props.Estado} - {props.Cidade}</h4>
+                <h4 className="text-center">{props.Cidade}</h4>
+                <h4>{props.Estado}</h4>
                 <img className="card-img" src={props.Picture} alt={'Imagem de' + props.Cidade}/>
                 <div>
                     <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Placeat itaque neque iure nostrum eius nemo, officiis nisi saepe delectus, cum est unde maxime voluptate quibusdam explicabo culpa a quos earum.</p>
--- src/Pages/Promocoes/Promocoes.jsx
+++ src/Pages/Promocoes/Promocoes.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import Card from '../../Components/Card/Card';
 import ibitipoca from '../../Images/ibitipoca.jpg'
+import floripa from '../../Images/floripa.webp'
 
 
 function Promocoes () {
@@ -9,9 +10,9 @@ function Promocoes () {
             <h1 className='py-5 text-center'>Destinos</h1>
             <div className='container d-flex'>
              <Card Estado={"Rio de Janeiro"} Cidade={"Ibitipoca"} Picture={ibitipoca} valor={"R$325,50"}></Card>
+             <Card Estado={"Santa Catarina"} Cidade={"Florianópolis"} Picture={floripa} valor={"R$457,80"}></Card>
              <Card Estado={"Rio de Janeiro"} Cidade={"Ibitipoca"} Picture={ibitipoca} valor={"R$325,50"}></Card>
-             <Card Estado={"Rio de Janeiro"} Cidade={"Ibitipoca"} Picture={ibitipoca} valor={"R$325,50"}></Card>
-             <Card Estado={"Rio de Janeiro"} Cidade={"Ibitipoca"} Picture={ibitipoca} valor={"R$325,50"}></Card>
+             <Card Estado={"Santa Catarina"} Cidade={"Florianópolis"} Picture={floripa} valor={"R$457,80"}></Card>
              
          </div>
         </React.Fragment>
